00ff0a487fbfba214f35c6eee7bd8c134005d62a
Izhakfc <izhakflores@gmail.com>
2023-03-26 20:06:54 -0600

    Adding an extra column called education level and creating a dummy variable to test appery connection"

--- schema.sql
+++ schema.sql
@@ -5,10 +5,26 @@ CREATE TABLE inventory (
 	garment_name VARCHAR(40) NOT NULL,
  	school_name VARCHAR(40) NOT NULL,
  	garment_size INTEGER NOT NULL,
+	education_level VARCHAR(40) NOT NULL,
  	qty INTEGER NOT NULL,
 	PRIMARY KEY (item_id),
 	 UNIQUE (item_id)
 );
 
 -- Query to confirm creation
+SELECT * FROM inventory;
+
+-- Dummy variable to test appery connection
+INSERT INTO inventory
+  (item_id, code, garment_name, school_name, garment_size, education_level, qty)
+VALUES
+  (1,
+   'PANT25DST0021D18SECU',
+   'PANTALON',
+   'SECUNDARIA TECNICA NUM 21',
+   18,
+   'SECUNDARIA',
+   1);
+
+-- Query to confirm insertion
 SELECT * FROM inventory;
